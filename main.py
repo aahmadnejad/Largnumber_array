929bb311a6891c48018a3ec7693d60530992cd0e
saba bagherinia <sababagherinia@gmail.com>
2021-07-15 04:07:24 -0700

    final function

--- main.py
+++ main.py
@@ -1,47 +1,17 @@
-def condition(key: int, num_: int):
-
-    str_num1 = str(key)
-    str_num2 = str(num_)
-
-    if int(str_num1[0]) > int(str_num2[0]):
-        result = True
-    elif int(str_num1[0]) == int(str_num2[0]) and len(str_num1) < len(str_num2):
-        result = True
-    else:
-        result = False
-
-    return result
-
-
-def compare(n, m):
-    if int(n[0]) > int(m[0]):
-        return True
-    elif int(n[0]) == int(m[0]) and len(n) <= len(m):
-        if len(n) > 1:
-            compare(n[1:], m[1:])
-        else:
-            return True
-    else:
-        return False
-
-
-def large_number(arr: list) -> int:
-    for i in range(1, len(arr)):
-
-        key = arr[i]
-
-        j = i - 1
-        while j >= 0 and compare(str(key), str(arr[j])):
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-
-    str_arr = [str(num_) for num_ in arr]
-
-    return int("".join(str_arr))
+def combine(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            num1 = str(arr[i]) + str(arr[j])
+            num2 = str(arr[j]) + str(arr[i])
+            if int(num1) > int(num2):
+                pass
+            else:
+                arr[i], arr[j] = arr[j], arr[i]
+    arr = [str(num_) for num_ in arr]
+    return int("".join(arr))
 
 
 if __name__ == "__main__":
     num = [44, 50, 6, 5, 9]
 
-    print(large_number(num))
+    print(combine(num))
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 import unittest
 
-from main import large_number
+from main import combine
 
 
 class Challenge2TestCase(TestCase):
@@ -16,13 +16,14 @@ class Challenge2TestCase(TestCase):
             ([2, 2, 2, 22, 222, 33, 202, 21], 332222222221202),
             ([3, 0, 8, 6, 7, 6, 0], 8766300),
             ([69699, 333, 68688], 6969968688333),
-            ([300, 311, 322, 355, 344, 30], 35534432231130300)
+            ([300, 311, 322, 355, 344, 30], 35534432231130300),
+            ([20, 200, 2000], )
 
         ]
 
     def test_numbers(self):
         for ts in self.test_cases:
-            self.assertEqual(large_number(ts[0]), ts[1])
+            self.assertEqual(combine(ts[0]), ts[1])
 
 
 if __name__ == "__main__":
